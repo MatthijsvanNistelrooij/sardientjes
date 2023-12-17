a8ef96a593bcdec016bbf9ea09285f57ff462832
Matthijs <matthijs.vannistelrooij@gmail.com>
2023-12-17 21:50:56 +0100

    resolved issues

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -6,7 +6,7 @@ type FileUploaderProps = {
   mediaUrl: string
 }
 
-const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+const FileUploader = ({ fieldChange }: FileUploaderProps) => {
   const [file, setFile] = useState<File[]>([])
   const [fileUrl, setFileUrl] = useState("")
 
